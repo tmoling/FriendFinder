64b4d26f02d6dbe633ec864ec090245ee9105d7f
Troy Moling <troy.moling@gmail.com>
2018-03-11 12:07:51 -0500

    adding new friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,41 +7,42 @@ var friends = [
 		"photo": "https://hairangle.com/wp-content/uploads/2016/12/coolguy.jpg",
 		"scores": [1,1,1,1,1,1,1,1,1,1]
 		//10
-	},
-	{
-		"name": "Brad",
-		"photo":"https://enciclopedia.us/wp-content/uploads/2018/02/1519336882-cool-guy-haircuts-2016-49-new-hairstyles-for-men-for-2016.jpg",
-		"scores": [5,5,5,5,5,5,5,5,5,5]
-		//55		
+  },
+  {
+		"name": "Ray",
+		"photo":"https://www.xing.com/image/7_6_5_32423d717_25827136_1/dieter-hoeller-foto.256x256.jpg",
+		"scores": [1,1,1,1,1,1,1,1,1,5]
+		//14		
+  },
+  {
+		"name": "Janet",
+		"photo":"http://www.irvingsjourney.com/wp-content/uploads/2014/08/woman_in_tears.jpg",
+		"scores": [3,2,1,2,1,1,3,1,1,3]
+		//18		
+  },
+  {
+		"name": "Rhoda",
+		"photo":"https://upload.wikimedia.org/wikipedia/commons/6/6e/Longhair_Tabby_JaJa.jpg",
+		"scores": [3,2,1,3,4,2,2,2,1,1]
+		//21		
   },
   {
 		"name": "Patty",
 		"photo":"https://images.gr-assets.com/authors/1232123231p8/2383.jpg",
-		"scores": [5,2,1,5,3,4,5,3,5,4]
-		//37		
-  },
-  {
-		"name": "Anna",
-		"photo":"https://i2.wp.com/mommyisworking.com/wp-content/uploads/2017/04/Cool-pic-of-woman.jpg",
-		"scores": [3,2,5,5,2,5,5,4,5,1]
-		//37		
-  },
-  {
-		"name": "Rhoda",
-		"photo":"https://www.transalt.org/sites/default/files/member-testimonial.jpg",
-		"scores": [3,5,1,3,4,2,3,3,4,1]
-		//29		
+		"scores": [2,2,1,5,3,3,2,3,3,1]
+		//25	
   },
   {
 		"name": "J.P.",
 		"photo":"https://www.splcenter.org/sites/default/files/51488.jpg",
 		"scores": [2,3,1,5,4,2,3,1,1,4]
 		//26	
-  },  {
-		"name": "Ray",
-		"photo":"https://www.xing.com/image/7_6_5_32423d717_25827136_1/dieter-hoeller-foto.256x256.jpg",
-		"scores": [4,3,3,2,1,2,3,3,4,2]
-		//27		
+  },  
+  {
+		"name": "Phillip",
+		"photo":"https://avatars.schd.ws/3/1D/4002150/avatar.jpg.320x320px.jpg",
+		"scores": [1,3,5,2,4,1,3,5,2,3]
+		//29		
   },
   {
 		"name": "Erika",
@@ -50,16 +51,46 @@ var friends = [
 		//30		
   },
   {
-		"name": "Phillip",
-		"photo":"https://avatars.schd.ws/3/1D/4002150/avatar.jpg.320x320px.jpg",
-		"scores": [1,3,5,2,4,1,3,5,2,3]
-		//29		
+		"name": "Scooter",
+		"photo":"https://i.pinimg.com/originals/08/81/ee/0881eeae672e1ac13cb1dd290bfb106a.jpg",
+		"scores": [4,4,3,4,5,3,3,3,3,2]
+		//34	
+  },
+  {
+		"name": "Anna",
+		"photo":"https://i2.wp.com/mommyisworking.com/wp-content/uploads/2017/04/Cool-pic-of-woman.jpg",
+		"scores": [3,2,5,5,2,5,5,4,5,1]
+		//37		
   },
   {
 		"name": "Mateo",
 		"photo":"https://avatars.schd.ws/C/55/4237270/avatar.jpg.320x320px.jpg",
-		"scores": [2,4,1,1,4,2,1,2,1,3]
-		//21		
+		"scores": [4,4,4,4,4,4,4,4,4,4]
+		//40	
+  },
+  {
+		"name": "Gene",
+		"photo":"http://img.izismile.com/img/img7/20140520/640/real_people_looking_and_acting_really_weird_640_64.jpg",
+		"scores": [5,1,3,4,5,4,5,5,5,5]
+		//42
+  },
+  {
+		"name": "Cody",
+		"photo":"http://i.imgur.com/0CwGq.png",
+		"scores": [5,4,5,4,5,4,5,4,5,4]
+		//45	
+  },
+  {
+		"name": "Sheila",
+		"photo":"https://www.ripleys.com/wp-content/uploads/2014/05/history-culture_s01.jpg",
+		"scores": [4,4,5,5,5,5,5,5,5,5]
+		//48	
+  },
+  {
+		"name": "Brad",
+		"photo":"https://enciclopedia.us/wp-content/uploads/2018/02/1519336882-cool-guy-haircuts-2016-49-new-hairstyles-for-men-for-2016.jpg",
+		"scores": [5,5,5,5,5,5,5,5,5,5]
+		//50		
   }
 ];
 
